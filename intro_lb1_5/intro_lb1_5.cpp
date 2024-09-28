d6ca116b0cbc14000767ac866e3eab1fe603bef9
Mykyta Parus <105989712+Nikita-Parus@users.noreply.github.com>
2024-09-28 11:00:31 +0300

    Add files via upload

---
+++ intro_lb1_5/intro_lb1_5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, ".1251");
+    int score;
+    cout << "";
+    cin >> score;
+    if (score >= 90){
+        cout << "Вiдмiнно" << endl;
+    }
+    else if (score < 90 && score >= 70) {
+        cout << "Добре" << endl;
+    }
+    else if (score < 70 && score >= 60) {
+        cout << "Задовiльно" << endl;
+    }
+    else {
+        cout << "Незадовiльно" << endl;
+    }
+}
